8a3d4ee5ee3afb4699eed3d3cc0252ccb6fdaf08
veggieboy <meanomenestcaesar@gmail.com>
2014-11-19 18:13:30 -0700

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,47 @@
-// A Point object represents a pair of (x, y) integer coordinates.
-// Reference Building Java Programs pp 505-517
+public class Point
+{
+  int x;
+  int y;
+  
+  public int getX()
+  {
+    return x;
+  }
+  public int getY()
+  {
+    return y;
+  }
+  public double distanceFromOrigin()
+  {
+    double h=Math.sqrt(x*x+y*y);
+    return h;
+  }
+  public double distance(Point b)
+  {
+    b.x2 = b;
+    b.y2 = b;
+    b.x2=x2;
+    b.y2=y2;
+    double h=Math.sqrt((x2–x)*(x2-x) + (y2–y)*(y2-y));
+    return h;
+  }
+  public String toString()
+  {
+    return("("+x+","+y+")");
+  }
+  public void setX (int xNew)
+  {
+    x=xNew;
+  }
+  public void setY (int yNew)
+  {
+    y=yNew;
+  }
+  public void setY (int dx, int dy)
+  {
+    x+=dx;
+    x+=dy;
+  }
+}
+
+
